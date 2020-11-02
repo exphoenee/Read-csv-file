1ce1d8e8a9d00ee75cdc55ee3f789a543461d0af
exphoenee <61277296+exphoenee@users.noreply.github.com>
2020-11-02 14:22:25 +0100

    table rendering improved

--- main.js
+++ main.js
@@ -6,6 +6,29 @@ class dataTable {
       header: [],
       rowNumber: null,
       rows: [],
+      columnsToSummarize: ["munkaertek", "mennyiseg"],
+      currencyNames: ["Ft", " EUR", "USD", "HUF", "CHF"],
+      specialChars: {
+        é: "e",
+        á: "a",
+        ó: "o",
+        ö: "o",
+        ő: "o",
+        ú: "u",
+        ü: "u",
+        ű: "u",
+        í: "i",
+        " ": "_",
+        É: "E",
+        Á: "A",
+        Ó: "O",
+        Ö: "O",
+        Ő: "O",
+        Ú: "U",
+        Ü: "U",
+        Ű: "U",
+        Í: "I",
+      },
     };
   }
 
@@ -42,90 +65,162 @@ class dataTable {
   }
 
   creatTable(rawData) {
+    let that = this;
     let allLines = rawData.split(/\r\n|\n/);
-    let header = allLines.shift().split(";");
 
-    this.table.header = header;
+    this.table.header = allLines.shift().split(";");
     this.table.rowNumber = allLines.length;
 
     for (let line of allLines) {
       this.table.rows.push(line.split(";"));
     }
 
-    visualizeData(this.table.header, this.table.rows);
+    renderTable(this.table.header, this.table.rows);
+
+    function renderTable(header, rows) {
+      let headerClass = [];
+      let summarizeCol = [];
+      let summary = [0];
+      let table = `
+        <table class="dataDatble">
+          <thead>
+            <tr class="headerRow">`;
+
+      for (let [i, col] of header.entries()) {
+        let colName = noSpecChars(col + "-" + i);
+
+        let summarize = false;
+        for (let columnToSummarize of that.table.columnsToSummarize) {
+          if (colName.includes(columnToSummarize)) {
+            summarize = true;
+            break;
+          }
+        }
+        summary[i] = 0;
+        summarizeCol.push(summarize);
+
+        table += `
+              <th class="${colName}${summarize ? " summarize" : ""}">${col}`;
+
+        headerClass.push(colName);
+      }
+
+      table += `
+            </tr>
+          </thead>
+          <tbody>`;
+
+      for (let [i, row] of rows.entries()) {
+        let tableRows = "";
+
+        tableRows += `
+            <tr class="${"row-" + i}">`;
+
+        let emptyCell = 0;
+
+        for (let [j, col] of row.entries()) {
+          col.length == 0 ? emptyCell++ : emptyCell;
+
+          let cellData = checkCellValue(col);
+          console.log(col, cellData);
+
+          summarizeCol[j] ? (summary[j] += cellData.cellValue) : null;
+
+          tableRows += `
+            <td class="${headerClass[j]} ${cellData.type}">${cellData.col}</td>`;
+        }
+
+        tableRows += `
+            </tr>`;
+
+        if (emptyCell !== header.length) {
+          table += tableRows;
+        }
+      }
+
+      table += `
+          </tbody>
+          <tfoot>
+            <tr class="headerFoot">`;
+      for (let [j, col] of header.entries()) {
+        table += `
+            <td class="${headerClass[j]}">${col}: ${summary[j]}</td>`;
+      }
+      table += `
+            </tr>
+          </tfoot>
+        </table>`;
+      that.container.innerHTML = table;
+    }
+
+    function checkCellValue(col) {
+      let value;
+
+      value = checkCurrency(col);
+      if (value) {
+        console.log(col, value);
+        return value;
+      }
+
+      value = checkNumber(col);
+      if (value) {
+        console.log(col, value);
+        return value;
+      }
+
+      return { type: "unknown", col: col, cellValue: cellValue };
+    }
+
+    function checkNumber(col) {
+      let cellData;
+      if (col.includes(",") || col.includes(".")) {
+        cellData = replaceAll(col, " ", "");
+        cellData = Number(col.replace(",", "."));
+        if (typeof cellData === "number" && !isNaN(cellData)) {
+          col = `
+            <span
+              class="value${typeof cellData !== "number" ? "-error" : ""}"
+              data-value="${cellData}"
+              data-type="${typeof cellData}"
+            >${cellData}</span>`;
+        }
+      }
+      return { type: "number", col: col, cellData: cellData };
+    }
+
+    function checkCurrency(col) {
+      for (let currencyName of that.table.currencyNames) {
+        let cellData;
+        if (col.includes(currencyName)) {
+          col = col.replace(currencyName, "");
+          col = replaceAll(col, " ", "");
+          cellData = Number(col);
+          col = `
+            <span
+              class="value${typeof cellData !== "number" ? "-error" : ""}"
+              data-currency="${currencyName}"
+              data-value="${cellData}"
+              data-type="${typeof cellData}"
+            >${cellData}</span>
+            <span class="currency">${currencyName}</span>`;
+          if (typeof cellData !== "number") {
+            cellData = 0;
+          }
+          return { type: "currency", col: col, cellData: cellData };
+        }
+      }
+      return null;
+    }
 
     function replaceAll(string, search, replace) {
       return string.split(search).join(replace);
     }
 
-    function visualizeData(header, rows) {
-      let table = createTable();
-      createHeader(table, header);
-      createRows(table, rows, header);
-    }
-
     function noSpecChars(text, lowercase = true) {
-      const charLookup = {
-        é: "e",
-        á: "a",
-        ó: "o",
-        ö: "o",
-        ő: "o",
-        ú: "u",
-        ü: "u",
-        ű: "u",
-        í: "i",
-        " ": "_",
-        É: "E",
-        Á: "A",
-        Ó: "O",
-        Ö: "O",
-        Ő: "O",
-        Ú: "U",
-        Ü: "U",
-        Ű: "U",
-        Í: "I",
-      };
-      for (let char in charLookup) {
-        text = replaceAll(text, char, charLookup[char]);
+      for (let char in that.table.specialChars) {
+        text = replaceAll(text, char, that.table.specialChars[char]);
       }
       return lowercase ? text.toLowerCase() : text;
-    }
-
-    function createTable() {
-      const table = document.createElement("table");
-      table.classList.add("dataTable");
-      document.body.appendChild(table);
-      return table;
-    }
-
-    function createHeader(table, header) {
-      const tblHead = document.createElement("tr");
-      tblHead.classList.add("header");
-      table.appendChild(tblHead);
-      for (let [i, col] of header.entries()) {
-        let headerRow = document.createElement("th");
-        let colName = noSpecChars(col);
-        headerRow.classList.add(colName);
-        headerRow.innerHTML = col;
-        tblHead.appendChild(headerRow);
-      }
-    }
-
-    function createRows(table, rows, header) {
-      for (let [i, row] of rows.entries()) {
-        let tblRow = document.createElement("tr");
-        //console.log(row + ": " + i);
-        tblRow.classList.add("row-" + i);
-        table.appendChild(tblRow);
-        for (let [j, col] of row.entries()) {
-          let tblCol = document.createElement("td");
-          let colName = noSpecChars(header[j]);
-          tblCol.classList.add(colName);
-          tblCol.innerHTML = col;
-          tblRow.appendChild(tblCol);
-        }
-      }
     }
   }
 }
@@ -135,4 +230,3 @@ let booking = new dataTable({
   datafile: "booking.csv",
 });
 booking.init();
-//console.log(booking);
